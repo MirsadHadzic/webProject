25274f04bbaef20821bd12aa958103456899a172
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2023-03-26 05:57:41 +0200

    zavrseno spajanje s bazom i osnovne funkcionalnosti

---
+++ test_dao.php
@@ -0,0 +1,33 @@
+<?php
+// Instanca part
+require_once("rest/dao/ProjectDao.class.php");
+$user_dao = new UsersDao();
+
+// za get 
+$results = $user_dao->get_all();
+print_r($results);
+/*
+// za add
+$firstName = $_REQUEST['firstName'];
+$lastName = $_REQUEST['lastName'];
+$age = $_REQUEST['age'];
+
+$results = $user_dao->add($firstName, $lastName, $age);
+print_r($results);
+// print_r(" Dodao sam tog i tog u bazu.");
+
+// za update
+$firstName = $_REQUEST['firstName'];
+$lastName = $_REQUEST['lastName'];
+$age = $_REQUEST['age'];
+$id = $_REQUEST['id'];
+
+$results = $user_dao->update($firstName, $lastName, $age, $id);
+print_r($results);*/
+
+// za delete
+$id = $_REQUEST['id'];
+$results = $user_dao->delete($id);
+print_r($results);
+
+?>
