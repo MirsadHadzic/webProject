f429280812a1cf8611d0dfb860edcac9e930e3a4
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2023-04-01 05:33:51 +0200

    odradjen switch case za crud operacije

--- test_dao.php
+++ test_dao.php
@@ -1,5 +1,5 @@
 <?php
-// Instanca part
+/*// Instanca part
 require_once("rest/dao/ProjectDao.class.php");
 $user_dao = new UsersDao();
 
@@ -23,11 +23,46 @@ $age = $_REQUEST['age'];
 $id = $_REQUEST['id'];
 
 $results = $user_dao->update($firstName, $lastName, $age, $id);
-print_r($results);*/
+print_r($results);
 
 // za delete
 $id = $_REQUEST['id'];
 $results = $user_dao->delete($id);
-print_r($results);
+print_r($results);*/
+
+// Instanca part
+require_once("rest/dao/ProjectDao.class.php");
+$user_dao = new UsersDao();
+
+$type = $_REQUEST['type'];
+
+switch ($type) {
+    case 'add':
+        $firstName = $_REQUEST['firstName'];
+        $lastName = $_REQUEST['lastName'];
+        $age = $_REQUEST['age'];
+        $results = $user_dao->add($firstName, $lastName, $age);
+        //print_r($results);
+        break;
+    
+    case 'delete':
+        $id = $_REQUEST['id'];
+        $user_dao->delete($id);
+        print_r($results);
+        break;    
+
+    case 'update':
+        $firstName = $_REQUEST['firstName'];
+        $lastName = $_REQUEST['lastName'];
+        $age = $_REQUEST['age'];
+        $results = $user_dao->update($firstName, $lastName, $age);
+        //print_r($results);
+        break;    
+            
+    default:
+        $results = $user_dao->get_all();
+        print_r($results);
+        break;
+}
 
 ?>
