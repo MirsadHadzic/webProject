c539c3c4a1bd2e77c01ca94985ac52dd313b13b5
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2023-04-02 22:05:57 +0200

    m

--- rest/dao/ProjectDao.class.php
+++ rest/dao/ProjectDao.class.php
@@ -1,121 +1,76 @@
 <?php
 
-/**
- * Summary of UsersDao
- */
-class UsersDao
-{
+class ToDoDao{
 
-    /**
-     * Summary of conn
-     * @var
-     */
     private $conn;
 
-    /*
-    * Class constructor used to establish connection to db
-    */ 
-
     /**
-     * Summary of __construct
-     */
+    * Class constructor used to establish connection to db
+    */
     public function __construct(){
         try {
-            $servername = "localhost";
-            $username = "root";
-            $password = "root";
-            $schema = "lab3_db";
-
-            $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
-            // set the PDO error mode to exception
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-          } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-          }
+          $servername = "localhost";
+          $username = "root";
+          $password = "root";
+          $schema = "project";
+          $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+          // set the PDO error mode to exception
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          //echo "Connected successfully";
+        } catch(PDOException $e) {
+          echo "Connection failed: " . $e->getMessage();
+        }
     }
 
-    /*
-    * Method used to get all users from database  
-    */
     /**
-     * Summary of get_all
-     * @return array
-     */
+    * Method used to get all students from database
+    */
     public function get_all(){
-        $stmt = $this->conn->prepare("SELECT * FROM users");
+        $stmt = $this->conn->prepare("SELECT * FROM todo");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
     /**
     * Method used to get student by id from database
     */
     public function get_by_id($id){
-        $stmt = $this->conn->prepare("SELECT * FROM users WHERE id=:id");
+        $stmt = $this->conn->prepare("SELECT * FROM todo WHERE id=:id");
         $stmt->execute(['id' => $id]);
         return $stmt->fetchAll();
     }
 
-
-    /*
-    * Method used to add users to database  
-    * string $first_name: First name is the first name of the user
-    */
-    /*public function add($firstName, $lastName, $age){
-        $stmt = $this->conn->prepare("INSERT INTO users (firstName, lastName, age) 
-        VALUES ('$firstName', '$lastName', '$age')");
-        $result = $stmt->execute();
-    }*/
-
     /**
-     * Summary of add
-     * @param mixed $user
-     * @return mixed
-     */
-    public function add($user){
-        $stmt = $this->conn->prepare("INSERT INTO users (firstName, lastName, age) VALUES (:firstName, :lastName, :age)");
-        $stmt->execute($user);
-        $user['id'] = $this->conn->lastInsertId();
-        return $user;
+    * Method used to get add student to database
+    * string $first_name: First name is the first name of the student
+    */
+    public function add($todo){
+        $stmt = $this->conn->prepare("INSERT INTO todo (todo_name, todo_time) VALUES (:todo_name, :todo_time)");
+        $stmt->execute($todo);
+        $todo['id'] = $this->conn->lastInsertId();
+        return $todo;
     }
 
-    /*
-    * Method used to update users to database  
-    */
-    /*public function update($firstName, $lastName, $age, $id){
-        $stmt = $this->conn->prepare("UPDATE users SET firstName = '$firstName', 
-        lastName = '$lastName', age = '$age' WHERE id = $id");
-        $result = $stmt->execute();
-    }*/
-
     /**
-     * Summary of update
-     * @param mixed $user
-     * @param mixed $id
-     * @return mixed
-     */
-    public function update($user, $id){
-        $user['id'] = $id;
-        $stmt = $this->conn->prepare("UPDATE users SET firstName = :firstName, lastName= :lastName, age = :age WHERE id=:id");
-        $stmt->execute($user);
-        return $user;
+    * Method used to get update student from database
+    */
+    public function update($todo, $id){
+        $todo['id'] = $id;
+        $stmt = $this->conn->prepare("UPDATE todo SET todo_name = :todo_name, todo_time=:todo_time WHERE id=:id");
+        $stmt->execute($todo);
+        return $todo;
     }
 
-    /*
-    * Method used to delete users from database  
-    */
     /**
-     * Summary of delete
-     * @param mixed $id
-     * @return void
-     */
+    * Method used to get delete student from database
+    */
     public function delete($id){
-        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = ?");
-        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        $stmt = $this->conn->prepare("DELETE FROM todo WHERE id = :id");
+        $stmt->bindParam(':id', $id); #prevent SQL injection
         $stmt->execute();
     }
-
 }
 
-?>
+
+
+ ?>
--- rest/index.php
+++ rest/index.php
@@ -1,80 +1,67 @@
 <?php
-
-// ove dvije tacke su za Exit, da hocemo iz dao izaci, napravili bismo ovo, ../../vendor/autoload.php
-
 require "../vendor/autoload.php";
 require "dao/ProjectDao.class.php";
 
-// flight register se koristi da ne bi ponavljali ovu instancu klase ProjectDaoClass... 
-Flight::register('user_dao', "UsersDao");
+Flight::register('todo_dao', "ToDoDao");
 
 Flight::route("/", function(){
    echo "Hello from / route";
 });
 
-// We need to return data in JSON format, because that will be our communication between frontend
-// and backend
-Flight::route("GET /users", function(){
+Flight::route("GET /todo", function(){
    //echo "Hello from /students route";
-   //echo "Hello from /students route with name= ".$name"; $name se posalje kao parametar u funkciji
-   // kod ovoga iznad dodaje se /students/mirso  
    //$student_dao = new StudentsDao();
    //$results = Flight::student_dao()->get_all();
    //print_r($results);
-   // dodati u header-u postman  content type application/json
-   Flight::json(Flight::user_dao()->get_all());
-   /*$memory_usage = memory_get_usage(); // get current memory usage
-   echo "Current memory usage: " . $memory_usage . " bytes";*/
+   Flight::json(Flight::todo_dao()->get_all());
 });
 
-Flight::route("GET /user_by_id", function(){
-   Flight::json(Flight::user_dao()->get_by_id(Flight::request()->query['id']));
+Flight::route("GET /todo_by_id", function(){
+   Flight::json(Flight::todo_dao()->get_by_id(Flight::request()->query['id']));
 });
 
-Flight::route("GET /users/@id", function($id){
+Flight::route("GET /todo/@id", function($id){
    //echo "Hello from /students route";
    //$student_dao = new StudentsDao();
    //$results = $student_dao->get_by_id($id);
    //print_r($results);
-   Flight::json(Flight::user_dao()->get_by_id($id));
+   Flight::json(Flight::todo_dao()->get_by_id($id));
 });
 
-Flight::route("DELETE /users/@id", function($id){
+Flight::route("DELETE /todo/@id", function($id){
    //echo "Hello from /students route";
    //$student_dao = new StudentsDao();
-   Flight::user_dao()->delete($id);
-   Flight::json(['message' => "Student deleted successfully"]);
+   Flight::todo_dao()->delete($id);
+   Flight::json(['message' => "ToDo deleted successfully"]);
 });
-  
-Flight::route("POST /user", function(){
-    //echo "Hello from /students route";
-    //$student_dao = new StudentsDao();
-    // ovo getData da se ekstraktuju values iz arrayj-aj 
+
+Flight::route("POST /todo", function(){
+   //echo "Hello from /students route";
+  //$student_dao = new StudentsDao();
    $request = Flight::request()->data->getData();
    //$response = $student_dao->add($request);
-   Flight::json(['message' => "Student added successfully",
-                 'data' => Flight::user_dao()->add($request)
+   Flight::json(['message' => "ToDo added successfully",
+                 'data' => Flight::todo_dao()->add($request)
                 ]);
 });
 
-Flight::route("PUT /user/@id", function($id){
+Flight::route("PUT /todo/@id", function($id){
    //echo "Hello from /students route";
    //$student_dao = new StudentsDao();
-   $user = Flight::request()->data->getData();
+   $todo = Flight::request()->data->getData();
    //$response = $student_dao->update($student, $id);
-   Flight::json(['message' => "Student edit successfully",
-                 'data' => Flight::student_dao()->update($user, $id)
+   Flight::json(['message' => "ToDo edit successfully",
+                 'data' => Flight::todo_dao()->update($todo, $id)
                 ]);
 });
 
-Flight::route("GET /users/@name", function($name){
+Flight::route("GET /todo/@name", function($name){
    echo "Hello from /students route with name= ".$name;
 });
 
-Flight::route("GET /users/@name/@status", function($name, $status){
+Flight::route("GET /todo/@name/@status", function($name, $status){
    echo "Hello from /students route with name = " . $name . " and status = " . $status;
 });
 
-// ide uvijek, da bi se startalo uopste*/
 Flight::start();
 ?>
